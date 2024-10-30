af2d559e6c5889e135c621d2120a31b9417fbadf
Phillip Bridgeman <phillipbridgeman@gmail.com>
2024-10-30 18:57:10 -0500

    Started working on scrape_weather.py file with documentation and class definition.

--- scrape_weather.py
+++ scrape_weather.py
@@ -4,3 +4,11 @@ scrape_weather.py
 Description: This script will scrape the weather data from Environment Canada.
 Author: Phillip Bridgeman
 """
+
+import urllib.request as request
+from html.parser import HTMLParser
+from datetime import datetime
+
+class WeatherScraper:
+    def __init__(self):
+        self.url = "https://weather.gc.ca/"
